616d9858611e92da897d5a0a8fe234e1ceb29fb6
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2023-11-10 14:40:13 +0800

    Fixing github renaming issue AddToWishListButton 1

--- src/features/product/components/Actions.tsx
+++ src/features/product/components/Actions.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import Ratings from './Ratings'
-import AddToWishlistButton from '../../../components/AddToWishListButton'
+import AddToWishlistButton from '../../../components/AddToWish ListButton'
 import ShareButton from '../../../components/ShareButton'
 import { ProductModel } from '../../../data/productModel'
 
